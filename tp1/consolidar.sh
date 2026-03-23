4b40f14726a93988c8f44185730f88dcb3cae2d8
lucas <lmino@fi.uba.ar>
2026-03-23 16:25:13 -0300

    agrego validacion de opcion 2 carpeta entrada sin contenido

--- tp1/consolidar.sh
+++ tp1/consolidar.sh
@@ -2,6 +2,13 @@
 path_entrada="$HOME/EPNro1/entrada"
 path_procesado="$HOME/EPNro1/procesado"
 
+contenido=$(ls -A $path_entrada)
+
+if [ -z $contenido]; then 
+    echo "No hay archivos en $path_entrada"
+    exit 1
+fi
+
 for file in "$path_entrada"/*; do
     if [ -d $file ]; then continue; fi
     while read LINE; do
